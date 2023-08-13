38af695ab7ffb3da239b7951dd39028778be9c2e
buijennyh <109253137+buijennyh@users.noreply.github.com>
2023-08-13 14:58:39 -0500

    Hi World

---
+++ ShawnBossApp/src/Message.tsx
@@ -0,0 +1,9 @@
+// functions are PascalCase
+function Message() {
+    // JSX - JavaSCript XML
+    const name = "ShawnBoss"
+    return <h1>Hello {name}!</h1>;
+    // To see how it translates to JS - use babeljs.io 
+}
+
+export default Message; 

5bb158f0bcf4ed6700b0cf166dfd50ccdd60c322
buijennyh <109253137+buijennyh@users.noreply.github.com>
2023-08-24 21:56:05 -0500

    centered Video

--- my-app/src/components/YouTubeVideo.tsx
+++ my-app/src/components/YouTubeVideo.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import styles from './YouTubeVideo.module.css';
 
 interface VideoProps {
   videoId: string;
 }
-
+ 
 const YouTubeVideo: React.FC<VideoProps> = ({ videoId }) => {
   const embedUrl = `https://www.youtube.com/embed/${videoId}`;
 
   return (
-    <div className="video-container">
+    <div className={styles["video-container"]}>
       <iframe
         title={`YouTube Video ${videoId}`}
         width="560"
